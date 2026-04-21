8e52dbc44ce81992fb8df9b59f92addde01ed0b5
quantrixlabs-ops <quantrixlabs@gmail.com>
2026-04-21 13:13:40 +0530

    Force Vercel rebuild - minor code style update

--- index.tsx
+++ index.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from './context/ThemeContext';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
